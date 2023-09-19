60414e3881623b95fb1f07e6b88155ad6ced95a2
abdullahtaha262 <abdullahtaha262@gmail.com>
2023-09-19 13:55:25 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void print_rev(char *s);
+int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif /* MAIN_H */
